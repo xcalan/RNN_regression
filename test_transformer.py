87b0875e34dcc6d3e3e0b1a1fb8920be0d671c58
xiecheng <1519955707@qq.com>
2021-07-07 21:28:19 +0800

    增加transformer时序预测代码

---
+++ test_transformer.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+"""
+@Time    : 2021/7/7 20:45
+@Author  : Xie Cheng
+@File    : test_transformer.py
+@Software: PyCharm
+@desc: transformer 测试
+"""
+import torch
+from torch import nn
+import numpy as np
+import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
+
+from myfunction import MyDataset
+
+# device GPU or CPU
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# print('You are using: ' + str(device))
+
+# batch size
+batch_size_test = 20
+
+# 导入数据
+filename = './data/data.csv'
+dataset_test = MyDataset(filename)
+test_loader = DataLoader(dataset_test, batch_size=batch_size_test, shuffle=False, drop_last=True)
+
+criterion = nn.MSELoss()  # mean square error
+
+
+# rnn 测试
+def test_rnn():
+    net_test = torch.load('.\\model\\tf_model.pkl')  # load model
+    test_loss = 0
+    net_test.eval()
+    with torch.no_grad():
+        for idx, (sin_input, cos_output) in enumerate(test_loader):
+            sin_input_np = sin_input.numpy()  # 1D
+            sin_input_torch = torch.from_numpy(sin_input_np[np.newaxis, :, np.newaxis])  # 3D
+
+            prediction = net_test(sin_input_torch.to(device))  # torch.Size([batch size])
+
+            if idx == 0:
+                predict_value = prediction
+                real_value = cos_output
+            else:
+                predict_value = torch.cat([predict_value, prediction], dim=0)
+                real_value = torch.cat([real_value, cos_output], dim=0)
+
+            loss = criterion(prediction, cos_output.to(device))
+            test_loss += loss.item()
+
+    print('Test set: Avg. loss: {:.9f}'.format(test_loss))
+    return predict_value, real_value
+
+
+if __name__ == '__main__':
+    # 模型测试
+    print("testing...")
+    p_v, r_v = test_rnn()
+
+    # 对比图
+    plt.plot(p_v.cpu(), c='green')
+    plt.plot(r_v.cpu(), c='orange', linestyle='--')
+    plt.show()
+    print("stop testing!")
+
